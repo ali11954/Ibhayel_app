5a8636c3cd395e7dbf2bc22bc20b9e9d0cc4c37d
aboghith993-eng <alimubark440@gmail.com>
2026-07-06 10:48:30 +0300

    Fix Render 500 errors: remove old server-side blueprints and alias rules that referenced non-existent endpoints

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,29 +1,9 @@
 from .auth import auth
-from .employees import employees_bp
-from .attendance import attendance_bp
-from .companies import companies_bp
-from .evaluations import evaluations_bp
-from .financial import financial_bp
-from .accounts import accounts_bp
-from .suppliers import suppliers_bp
-from .reports import reports_bp
-from .labor import labor_bp
-from .settings import settings_bp
 from .api import api_bp
 from .rest_api import rest_api
 
 ALL_BLUEPRINTS = [
     auth,
-    employees_bp,
-    attendance_bp,
-    companies_bp,
-    evaluations_bp,
-    financial_bp,
-    accounts_bp,
-    suppliers_bp,
-    reports_bp,
-    labor_bp,
-    settings_bp,
     api_bp,
     rest_api,
 ]
